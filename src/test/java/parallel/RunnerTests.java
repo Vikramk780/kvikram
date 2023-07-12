ab1be2bf9b827cc8d7673e590ec624d7c4ac1180
Vikramk780 <55432495+Vikramk780@users.noreply.github.com>
2023-07-12 21:15:43 +0530

    Update RunnerTests.java

--- src/test/java/parallel/RunnerTests.java
+++ src/test/java/parallel/RunnerTests.java
@@ -9,7 +9,8 @@ features = "./src/test/resources/parallel",
 glue = "parallel", //this parallel relavant to applicationhooks
 monochrome =true,
 dryRun =false,
-plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+	  "timeline:test-output-thread/"
 
 
 		
